f0ee3a530fc8b1b9aecb3f335cd1cd338435f025
whugo43 <whugo43@hotmail.com>
2017-06-24 14:46:05 -0500

    Lista_Conteo comentada

--- src/Lista_Conteo.c
+++ src/Lista_Conteo.c
@@ -1,7 +1,8 @@
 /*Este metodo determina el numero de elemento de la lista 
-@param *lista apunta a null  significa que esta vacio. 
 
-@return un entero 0 si la lista esta vacia o caso contrario el componente numeroElementos de la lista retornara en lunero de elementos de la lista.     
+@param *lista apunta a null  significa que esta vacio. 
+@return un entero 0 si la lista esta vacia o caso contrario el componente numeroElementos de la lista retornara el nunero de elementos de la lista.     
+
 @author whugo43                    */ 
 int Lista_Conteo(ListaEnlazada *lista){
 	if(lista==NULL ){
