2a6454430d3153f6a231f7d5993f8eedc645c86d
whugo43 <whugo43@hotmail.com>
2017-06-24 14:42:23 -0500

    Lista_Conteo comentada

--- src/Lista_Vacia.c
+++ src/Lista_Vacia.c
@@ -1,6 +1,5 @@
 /*Este metodo determina si la lista esta vacia 
-iniciliza los elementos de la estructura ListaEnlazada
-@param *lista apunta a null o l componente numeroElementos se inicializa es igual a cero. significa que esta vacio.
+@param *lista apunta a null o al componente numeroElementos se inicializa es igual a cero. significa que esta vacio.
 @return un entero 1 si la lista esta vacia o 0 en caso contrario     
 @author whugo43                    */ 
 
