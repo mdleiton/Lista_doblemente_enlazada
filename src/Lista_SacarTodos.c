d5e79239df4ddaaa1093fdab0bf699a8939dd602
whugo43 <whugo43@hotmail.com>
2017-06-24 15:29:18 -0500

    Lista_SacarTodos comentada

--- src/Lista_SacarTodos.c
+++ src/Lista_SacarTodos.c
@@ -1,3 +1,11 @@
+
+/*Este metodo saca todos los elementos de la estructura ListaEnlazada
+
+Si la lista no esta vacia diferente de null. 
+@param  *lista es un puntero a la lista a la cual inicializaremos. el componente ancla apunte a null, el componente numeroElementos se inicializa a cero, muy similiar al metodo Lista_Inicializar.
+
+@author whugo43                    */ 
+
 void Lista_SacarTodos(ListaEnlazada *lista){
 	if(lista!=NULL){
 		lista->ancla.siguiente=&(lista->ancla);
