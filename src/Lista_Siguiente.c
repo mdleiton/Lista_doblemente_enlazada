8a2ae530d915a73fb7a12e025044fd0129e96309
jhonnyzp <jhonnyzp@outlook.com>
2017-06-24 16:06:44 -0500

    Lista_Siguiente

---
+++ src/Lista_Siguiente.c
@@ -0,0 +1,8 @@
+ElementoLista *Lista_Siguiente(ListaEnlazada *lista, ElementoLista *elemento){
+	if(lista==NULL || elemento==NULL)
+		return NULL;
+	if(elemento==  Lista_Ultimo(lista)){
+    		return NULL;
+	}
+	return elemento->siguiente;
+}
