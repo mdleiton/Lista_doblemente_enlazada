1b9f8759deb4cc75aa10c7d72c3f523b98ebaf7c
whugo43 <whugo43@hotmail.com>
2017-06-24 15:21:27 -0500

    Lista_Ultimo comentada

--- src/Lista_Ultimo.c
+++ src/Lista_Ultimo.c
@@ -1,3 +1,9 @@
+/*Este metodo determina el elemento anterior de la lista 
+
+@param *lista apunta a null  significa que esta vacio,  
+@return un entero null si la lista esta vacia o caso contrario el componente anterior de la ancla retornara el elemento anterior de la lista.     
+
+@author whugo43   */
 ElementoLista *Lista_Ultimo(ListaEnlazada *lista){
 	if(lista==NULL){
 		return NULL;
