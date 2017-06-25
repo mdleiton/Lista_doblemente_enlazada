39de16a26e40a7e0cdab94b5d721d533cb85a34c
mdleiton <mau_leiton96@hotmail.com>
2017-06-24 20:55:56 -0500

    Lista_InsertarAntes  completada y comentada

--- src/Lista_Buscar.c
+++ src/Lista_Buscar.c
@@ -1,5 +1,5 @@
 /*Este metodo permite buscar un elemento de la estructura lista
-@param *lista es un puntero a la lista la cual deseamos sacar/eliminar un elemento
+@param *lista es un puntero a la lista la cual deseamos buscar un elemento
 @param *objeto es un puntero a un objeto que se desea almacenar en la estructura ListaEnlazada 
 @return *elementoLista  un elemento lista que contiene igual objeto que el parametro.
 @author mdleiton   */
---
+++ src/Lista_InsertarAntes.c
@@ -0,0 +1,30 @@
+/*Este metodo permite insertar un elemento de la estructura lista
+@param *lista es un puntero a la lista la cual deseamos insertar un elemento
+@param *objeto es un puntero a un objeto que se desea almacenar en la estructura ListaEnlazada 
+@param *elemento es un puntero a una estructura ElementoLista . antes de este elemento se inserta el nuevo
+@return un entero 1 si se realizo con exito la operacion o 0 en caso contrario 
+@author mdleiton                                     */ 
+int Lista_InsertarAntes(ListaEnlazada *lista, void *objeto, ElementoLista *elemento){
+	if(lista==NULL || elemento==NULL) //valida que los parametros sean diferentes de nulo
+		return 0;
+	ElementoLista *nuevo=NULL;
+	if ((nuevo = malloc (sizeof(ElementoLista))) == NULL){ //asignando espacio valido en memoria
+    	return 0;
+    }else{
+    	ElementoLista *insertprev= &(lista->ancla);
+    	while((insertprev->siguiente )!= &(lista->ancla)){ //verificamos que dicho elemento pertenezca a la lista
+			if(insertprev->siguiente == elemento){ 
+				nuevo->siguiente=elemento;
+		    	nuevo->anterior=elemento->anterior;
+		    	elemento->anterior=nuevo;
+		    	(nuevo->anterior)->siguiente=nuevo;
+		    	nuevo->objeto=objeto;
+    			lista->numeroElementos += 1;
+			return 1;
+			}
+			insertprev = insertprev->siguiente;
+		}
+    }
+	
+	return 0; // si no dicho elemento no pertenece a la lista
+}
