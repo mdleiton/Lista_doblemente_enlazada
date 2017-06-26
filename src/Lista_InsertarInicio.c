9533841f56c3c042f848849e3dcb389f055d72d1
hugo wong <whugo43@hotmail.com>
2017-06-25 20:39:52 -0500

    funcion Lista_InsertarInicio comentada

--- src/Lista_InsertarInicio.c
+++ src/Lista_InsertarInicio.c
@@ -1,18 +1,23 @@
 #include "miLista.h"
 #include <stdlib.h>
 #include<stdio.h>
+/*Este metodo permite insertar un elemento a la estructura lista
+@param *lista es un puntero a la lista la cual deseamos ingresar un nuevo elemento
+@param *objeto es un puntero al objeto que se desea almacenar en la estructura ListaEnlazada 
+@return un entero 1 si se realizo con exito la operacion o 0 en caso contrario 
+@author mdleiton1                                      */ 
 int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto){
 	if(lista==NULL)
 		return 0;
 	ElementoLista *nuevo=NULL;
 	if ((nuevo = (ElementoLista *)malloc (sizeof(ElementoLista))) == NULL)
 		return 0;
-	if((lista->numeroElementos) == 0){
+	if((lista->numeroElementos) == 0){ //si la lista esta vacia debemos insertar al inicio . despues de ancla
 		nuevo->siguiente=&(lista->ancla);
 		nuevo->anterior = &(lista->ancla);
 		(lista->ancla).siguiente=nuevo;
 		(lista->ancla).anterior=nuevo;
-	}else{
+	}else{ //agregamos el elemento al final de la lista
 		ElementoLista *inicio=lista->ancla.siguiente;
 		nuevo->siguiente=inicio;
 		nuevo->anterior = &(lista->ancla);
