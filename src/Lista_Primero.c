dada2cc5c02a0d8671cfc0244cd2644c88018818
hugo wong <whugo43@hotmail.com>
2017-06-25 20:37:19 -0500

    Funcion Lista_Primero comentada

--- src/Lista_Primero.c
+++ src/Lista_Primero.c
@@ -1,6 +1,10 @@
 #include "miLista.h"
 #include <stdlib.h>
 #include<stdio.h>
+/*Este metodo determina el elemento sifuiente de la lista 
+@param *lista apunta a null  significa que esta vacio,  
+@return un entero null si la lista esta vacia o caso contrario el componente siguiente de la ancla retornara el elemento siguiente de la lista.     
+@author whugo43   */
 ElementoLista *Lista_Primero(ListaEnlazada *lista){
 	if(lista==NULL ){
 		return NULL;
